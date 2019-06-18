e182a49d9334d349c5a4f09671d648ebc6c6aab8
Vinayak Hegde <vinayak.hegde@playtech.com>
2019-06-18 17:11:09 +0100

    Updates

---
+++ precache-manifest.75d53272b6801819beccc313a411f0d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbaf0e04a7d93b297abfec5a440ec20d",
+    "url": "/booking-search-autocomplete/index.html"
+  },
+  {
+    "revision": "1cce90b0f9eb056dc398",
+    "url": "/booking-search-autocomplete/static/css/main.d46f74f8.chunk.css"
+  },
+  {
+    "revision": "e711f691d3cd19de11b4",
+    "url": "/booking-search-autocomplete/static/js/2.fcad81e9.chunk.js"
+  },
+  {
+    "revision": "1cce90b0f9eb056dc398",
+    "url": "/booking-search-autocomplete/static/js/main.d71672ad.chunk.js"
+  },
+  {
+    "revision": "970107456967e78a8927",
+    "url": "/booking-search-autocomplete/static/js/runtime~main.d8e28815.js"
+  }
+]);
